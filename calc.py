f1a445e528d4a9bed11342a740c2f4c6081f56ba
Lucas Oshiro <lucasseikioshiro@gmail.com>
2020-07-20 23:07:59 -0300

    banana

--- calc.py
+++ calc.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 from sys import argv
+from math import log
+
+ops = 0x2B
 
 def func(a, b):
-    pass
+    return '\n'.join(
+        (lambda r: f'{a} {f} {b} = {r}')(eval(f'{a}{f}{b}'))
+        for f in ops.to_bytes((int(log(ops, 16)) + 1) // 2, 'big').decode())
+    
 
 if __name__ == '__main__':
     a, b = map(int, argv[1:])
-    func(a, b)
+    print(func(a, b))
